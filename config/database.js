9306ad880396fe0c186d7e2cd4796f872f00d474
Komoditas Lerep <alfrethanovleonhardt@gmail.com>
2023-12-12 00:29:05 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('capstone', 'postgres', 'justkidd1234', {
+const db = new Sequelize('capstone', 'postgres', '123', {
     host : 'localhost',
     dialect : 'postgres'
 })
